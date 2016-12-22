6ed36758e0e9ed0b89eb59892ffc9ffaa7a76612
maguohua <1264889788@qq.com>
2016-12-22 09:31:59 +0800

    updata

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import ajax from './config/ajax'
 import './style/common'
 import './config/rem'
 
+ajax('GET', '/shopping/restaurants?latitude=31.16407&longitude=121.38876&offset=0&limit=20&extras[]=activities')
+
+
+//ajax('GET', '/v2/index_entry?geohash=wtw3630xg5e&group_type=1&flags[]=F')
+
+
 Vue.use(VueRouter)
 const router = new VueRouter({
 	routes
