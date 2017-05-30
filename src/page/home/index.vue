ab432c9d126036a3a53d2ced4da48564cd90f89f
Sisquoc <sisquoc@foxmail.com>
2017-05-30 13:48:19 +0800

    update
    1. mutations.js中只放同步函数，异步操作放action.js中
    2. 删除部分无用代码
    3. pug、stylus分别重写html和css

--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -1,25 +1,18 @@
-<template>
-  	<div class="home_container">
-        <itemcontainer father-component="home"></itemcontainer>
-    </div>
+<template lang="pug">
+.home_container
+  itemcontainer(father-component="home")
 </template>
 
 <script>
 import itemcontainer from '../../components/itemcontainer'
-
 export default {
-	name: 'home',
-  	components: {
-  		itemcontainer
-  	},
-    created(){
-        
-    }
+  name: 'home',
+  components: {
+      itemcontainer
+  }
 }
 </script>
 
-<style lang="less" scoped>
-    .home_container{
+<style>
 
-    }
 </style>
--- src/page/item/index.vue
+++ src/page/item/index.vue
@@ -1,25 +1,21 @@
-<template>
-  	<div>
-    	<itemcontainer father-component="item"></itemcontainer>
-  	</div>
+<template lang="pug">
+div
+  itemcontainer(father-component="item")
 </template>
 
 <script>
 import itemcontainer from '../../components/itemcontainer'
-
 export default {
-	name: 'item',
-  	components: {
-   		itemcontainer
-  	},
-  	created(){
-  		this.$store.commit('REMBER_TIME');
-  	}
+  name: 'item',
+  components: {
+      itemcontainer
+  },
+  created() {
+      this.$store.dispatch('remberTime');
+  }
 }
-
 </script>
 
 <style lang="less">
-    
 
 </style>
