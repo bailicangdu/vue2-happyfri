dc446ba35fb6d3a6bf7a67218341de4f198c97c8
mrjzhang(张志坚) <mrjzhang@tencent.com>
2017-11-09 21:20:50 +0800

    style: mutation declaration style

---
+++ src/store/mutation-types.js
@@ -0,0 +1,4 @@
+export const ADD_ITEMNUM = 'ADD_ITEMNUM'
+export const REMBER_ANSWER = 'REMBER_ANSWER'
+export const REMBER_TIME = 'REMBER_TIME'
+export const INITIALIZE_DATA = 'INITIALIZE_DATA'
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,8 +1,10 @@
-const ADD_ITEMNUM = 'ADD_ITEMNUM'
-const REMBER_ANSWER = 'REMBER_ANSWER'
-const REMBER_TIME = 'REMBER_TIME'
-const INITIALIZE_DATA = 'INITIALIZE_DATA'
-
+/*
+ * @Author: mrjzhang
+ * @Date: 2017-11-09 21:17:53
+ * @Last Modified by: mrjzhang
+ * @Last Modified time: 2017-11-09 21:19:11
+ */
+import * as types from './mutation-types'
 /**
  * Vuex 中的 mutation 非常类似于事件：每个 mutation 都有一个字符串的 事件类型 (type)
  * 和 一个 回调函数 (handler)。这个回调函数就是我们实际进行状态更改的地方，并且它会接受 state 作为第一个参数
@@ -22,17 +24,17 @@ export default {
 
   // 我们可以使用 ES2015 风格的计算属性命名功能来使用一个常量作为函数名
   // 点击进入下一题
-  [ADD_ITEMNUM](state, num) {
+  [types.ADD_ITEMNUM](state, num) {
     state.itemNum += num;
   },
   //记录答案
-  [REMBER_ANSWER](state, id) {
+  [types.REMBER_ANSWER](state, id) {
     state.answerid.push(id);
   },
   /*
   记录做题时间
    */
-  [REMBER_TIME](state) {
+  [types.REMBER_TIME](state) {
     state.timer = setInterval(() => {
       state.allTime++;
     }, 1000)
@@ -40,7 +42,7 @@ export default {
   /*
   初始化信息，
    */
-  [INITIALIZE_DATA](state) {
+  [types.INITIALIZE_DATA](state) {
     state.itemNum = 1;
     state.allTime = 0;
     state.answerid = [];
