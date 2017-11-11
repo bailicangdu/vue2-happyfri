1c7557d40262f905ebd79f00df4b196eb13f6f8e
mrjzhang(张志坚) <mrjzhang@tencent.com>
2017-11-11 10:03:50 +0800

    fix: webpack file source config

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../happyfri/index.html'),
     assetsRoot: path.resolve(__dirname, '../happyfri'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/happyfri/',
+    assetsPublicPath: '../happyfri/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
